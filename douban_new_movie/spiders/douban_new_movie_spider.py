6aef0a0914aa027df213b09f61c3dbd07b402462
xiang-su <271941476@qq.com>
2017-12-06 22:31:22 +0800

    适应 Scrapy Cloud

--- douban_new_movie/spiders/douban_new_movie_spider.py
+++ douban_new_movie/spiders/douban_new_movie_spider.py
@@ -4,29 +4,28 @@ import scrapy
 class DoubanNewMovieSpider(scrapy.Spider):
     name = "douban_new_movie_spider"
 
-    allowed_domains = ["www.movie.douban.com"]
+    allowed_domains = ["http://atjason.com"]
 
     start_urls = [
-        'https://movie.douban.com/chart',
+        'http://atjason.com',
     ]
 
     def parse(self, response):
         # yield scrapy.Request(response, callback=self.parse)
-        sel = scrapy.Selector(response,callback=self.parse)
+        sel = scrapy.Selector(response)
+        # sel = scrapy.Selector(response,callback=self.parse)
 
-        # movie_name=sel.xpath("//div[@class='wrapper']/div[@class='content']/div[@class='clearfix']/div[@class='indent']/div/table/tbody/tr[@class='item']/td[@valign='top']/div[@class='pl2']/a/text()").extract()
-        movie_name = sel.xpath("//div[@class='pl2']/a/text()").extract()
-        movie_url = sel.xpath("//div[@class='pl2']/a/@href").extract()
-        movie_star = sel.xpath("//div[@class='pl2']/div/span[@class='rating_nums']/text()").extract()
+        # title=sel.xpath("//div[@class='wrapper']/div[@class='content']/div[@class='clearfix']/div[@class='indent']/div/table/tbody/tr[@class='item']/td[@valign='top']/div[@class='pl2']/a/text()").extract()
+        title = sel.xpath("//*[@id='posts']/article/header/h1/a/text()").extract()
+        time = sel.xpath("//*[@id='posts']/article/header/div/span[1]/time").extract()
+        content = sel.xpath("//*[@id='posts']/article/div[1]/p/text()").extract()
 
-        # //*[@id="content"]/div/div[1]/div/div/table[1]/tbody/tr/td[2]/div/a
-        # //*[@id="content"]/div/div[1]/div/div/table[2]/tbody/tr/td[2]/div/a
         item = {}
 
-        item['movie_name'] = [n.encode('utf-8') for n in movie_name]
-        item['movie_star'] = [n for n in movie_star]
-        item['movie_url'] = [n for n in movie_url]
+        item['title'] = [n.encode('utf-8') for n in title]
+        item['content'] = [n for n in content]
+        item['time'] = [n for n in time]
 
         yield item
 
-        print(movie_name, movie_star, movie_url)
+        print(title,'\n', content,'\n', time)
