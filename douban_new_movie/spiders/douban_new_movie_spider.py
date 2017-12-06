5597edd8c6d10d7efe85cb682a68d203e7df2113
xiang-su <271941476@qq.com>
2017-12-06 20:16:20 +0800

    适应 Scrapy Cloud

--- douban_new_movie/spiders/douban_new_movie_spider.py
+++ douban_new_movie/spiders/douban_new_movie_spider.py
@@ -1,33 +1,34 @@
-from scrapy.spiders import Spider
 from scrapy.selector import Selector
+from scrapy.spiders import Spider
 
 from douban_new_movie.items import DoubanNewMovieItem
 
+
 class DoubanNewMovieSpider(Spider):
-	name="douban_new_movie_spider"
+    name = "douban_new_movie_spider"
 
-	allowed_domains=["www.movie.douban.com"]
+    allowed_domains = ["www.movie.douban.com"]
 
-	start_urls=[
-		'http://movie.douban.com/chart'
-	]
+    start_urls = [
+        'http://movie.douban.com/chart'
+    ]
 
-	def parse(self,response):
-		sel=Selector(response)
+    def parse(self, response):
+        sel = Selector(response)
 
-#		movie_name=sel.xpath("//div[@class='wrapper']/div[@class='content']/div[@class='clearfix']/div[@class='indent']/div/table/tbody/tr[@class='item']/td[@valign='top']/div[@class='pl2']/a/text()").extract()
-		movie_name=sel.xpath("//div[@class='pl2']/a/text()").extract()
-		movie_url=sel.xpath("//div[@class='pl2']/a/@href").extract()
-		movie_star=sel.xpath("//div[@class='pl2']/div/span[@class='rating_nums']/text()").extract()
+        # movie_name=sel.xpath("//div[@class='wrapper']/div[@class='content']/div[@class='clearfix']/div[@class='indent']/div/table/tbody/tr[@class='item']/td[@valign='top']/div[@class='pl2']/a/text()").extract()
+        movie_name = sel.xpath("//div[@class='pl2']/a/text()").extract()
+        movie_url = sel.xpath("//div[@class='pl2']/a/@href").extract()
+        movie_star = sel.xpath("//div[@class='pl2']/div/span[@class='rating_nums']/text()").extract()
 
-#		//*[@id="content"]/div/div[1]/div/div/table[1]/tbody/tr/td[2]/div/a
-#		//*[@id="content"]/div/div[1]/div/div/table[2]/tbody/tr/td[2]/div/a
-		item=DoubanNewMovieItem()
+        # //*[@id="content"]/div/div[1]/div/div/table[1]/tbody/tr/td[2]/div/a
+        # //*[@id="content"]/div/div[1]/div/div/table[2]/tbody/tr/td[2]/div/a
+        item = DoubanNewMovieItem()
 
-		item['movie_name']=[n.encode('utf-8') for n in movie_name]
-		item['movie_star']=[n for n in movie_star]
-		item['movie_url']=[n for n in movie_url]
+        item['movie_name'] = [n.encode('utf-8') for n in movie_name]
+        item['movie_star'] = [n for n in movie_star]
+        item['movie_url'] = [n for n in movie_url]
 
-		yield item
+        yield item
 
-		print movie_name,movie_star,movie_url
+        print(movie_name, movie_star, movie_url)
